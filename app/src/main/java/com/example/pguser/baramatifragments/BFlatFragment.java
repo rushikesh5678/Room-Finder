f4cf9649fb04337b9d8dd251221d997eadb2cacc
Rushikesh Mahajan <dm1408635@gmail.com>
2022-05-29 14:13:44 +0530

    User BaramatiFlatFragment

--- app/src/main/java/com/example/pguser/baramatifragments/BFlatFragment.java
+++ app/src/main/java/com/example/pguser/baramatifragments/BFlatFragment.java
@@ -32,7 +32,6 @@ public class BFlatFragment extends Fragment {
     RecyclerView mRecyclerView;
     List<PGData> pgData;
 
-
     ProgressDialog progressDialog;
 
     private ValueEventListener eventListener;
